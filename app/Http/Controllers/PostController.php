a447908f11be774f883027567349be442d2c7377
Kensuke Ota <40653830+KensukeOta@users.noreply.github.com>
2023-10-18 17:01:45 +0900

    記事投稿機能のAPIを実装 (#6)

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Post;
+use App\Http\Requests\PostRequest;
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    public function store(PostRequest $request)
+    {
+        $post = Post::create([
+            'title' => $request->title,
+            'body' => $request->body,
+            'user_id' => $request->user_id,
+        ]);
+
+        return response()->json(['post' => $post], 201);
+    }
+}
