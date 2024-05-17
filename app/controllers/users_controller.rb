c76f33e67b438a6df8bab0cd57262f14ae6bd56d
KensukeOta <bump_kensuke_miwa@i.softbank.jp>
2024-05-18 04:04:33 +0900

    ルーティングを記述し、渡されてきたメールアドレスからユーザー情報を返すアクションを実装 (#1)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,13 @@
 class UsersController < ApplicationController
+  def show_by_email
+    @user = User.find_by(email: params[:email])
+    if @user
+      render json: @user, status: :ok
+    else
+      render json: { error: "User not found" }, status: :not_found
+    end
+  end
+  
   def create
     @user = User.find_or_create_by(user_params)                      
     if @user
--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,6 @@ Rails.application.routes.draw do
   # root "posts#index"
   scope "v1/api" do
     resources :users
+    get "/users/email/:email", to: "users#show_by_email", constraints: { email: /[^\/]+/ }
   end
 end
