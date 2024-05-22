cd745e2690d8d860ef2af5b166e775c46ae78a26
KensukeOta <bump_kensuke_miwa@i.softbank.jp>
2024-05-23 00:18:48 +0900

    該当の記事を１件返すshowアクションを実装 (#5)

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,8 +1,14 @@
 class PostsController < ApplicationController
   def index
-    @posts = Post.order(created_at: :desc).includes(:user)
+    @posts = Post.order(created_at: :desc)
     
-    render json: { allPosts: @posts }, include: :user
+    render json: { allPosts: @posts }, status: :ok, include: :user
+  end
+
+  def show
+    @post = Post.find(params[:id])
+
+    render json: { post: @post }, status: :ok, include: :user
   end
   
   def create
