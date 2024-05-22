81345608f1006d95294bd99f697c812ae31bdfd4
KensukeOta <bump_kensuke_miwa@i.softbank.jp>
2024-05-23 00:56:51 +0900

    includesメソッドを使用して関連するユーザーをプリロードし、パフォーマンスを向上

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,12 +1,12 @@
 class PostsController < ApplicationController
   def index
-    @posts = Post.order(created_at: :desc)
+    @posts = Post.includes(:user).order(created_at: :desc)
     
     render json: { allPosts: @posts }, status: :ok, include: :user
   end
 
   def show
-    @post = Post.find(params[:id])
+    @post = Post.includes(:user).find(params[:id])
 
     render json: { post: @post }, status: :ok, include: :user
   end
