d213d4483ef407450a1ec443ec1722bd0502c049
KensukeOta <bump_kensuke_miwa@i.softbank.jp>
2024-06-09 23:33:05 +0900

    usersテーブルにpassword_digestカラムを追加し、bcryptをインストールし、has_secure_passwordメソッドを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_05_16_085601) do
+ActiveRecord::Schema[7.1].define(version: 2024_06_09_125656) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -31,6 +31,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_16_085601) do
     t.string "provider"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
   add_foreign_key "posts", "users"
