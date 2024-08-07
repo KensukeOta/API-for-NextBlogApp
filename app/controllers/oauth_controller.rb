fc6aa4101568d423a7073dd90183797b15ad5083
KensukeOta <bump_kensuke_miwa@i.softbank.jp>
2024-08-07 15:50:48 +0900

    OAuth認証ユーザー登録処理で、既存ユーザーの場合、そのユーザーを返すよう変更

--- app/controllers/oauth_controller.rb
+++ app/controllers/oauth_controller.rb
@@ -3,19 +3,18 @@ class OauthController < ApplicationController
     # ユーザーが既に存在するかをメールアドレスとプロバイダーで検索
     @user = User.find_by(email: user_params[:email], provider: user_params[:provider])
 
-    # 既存ユーザーが見つかった場合、処理を終了
+    # 既存ユーザーが見つかった場合、ユーザーを返す
     if @user
+      render json: @user, status: :ok
       return
     end
     
     # 新規ユーザーが見つからなかった場合、初期化
-    unless @user
-      @user = User.new(user_params)
-      # OAuth認証の際、実際のパスワードは不要なのでランダムなパスワードを設定
-      @user.password = SecureRandom.hex(10)
-      # パスワード確認用に同じ値を設定
-      @user.password_confirmation = @user.password
-    end
+    @user = User.new(user_params)
+    # OAuth認証の際、実際のパスワードは不要なのでランダムなパスワードを設定
+    @user.password = SecureRandom.hex(10)
+    # パスワード確認用に同じ値を設定
+    @user.password_confirmation = @user.password
 
     if @user.save
       render json: @user, status: :created
