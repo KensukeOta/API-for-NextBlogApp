7e5fcdf4d3a5996fdf01fca35700dbf0dfc2bf5a
KensukeOta <bump_kensuke_miwa@i.softbank.jp>
2024-08-22 11:12:37 +0900

    Likeモデルを作成し、リレーションを実装し、バリデーションを実装し、likesテーブルを作成 (#13)

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_06_09_125656) do
+ActiveRecord::Schema[7.1].define(version: 2024_08_22_012922) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "likes", force: :cascade do |t|
+    t.bigint "post_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_likes_on_post_id"
+    t.index ["user_id", "post_id"], name: "index_likes_on_user_id_and_post_id", unique: true
+    t.index ["user_id"], name: "index_likes_on_user_id"
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
@@ -34,5 +44,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_06_09_125656) do
     t.string "password_digest"
   end
 
+  add_foreign_key "likes", "posts"
+  add_foreign_key "likes", "users"
   add_foreign_key "posts", "users"
 end
