ff18f286ff1fffee26040dc828326d5fbe4bb66d
KensukeOta <bump_kensuke_miwa@i.softbank.jp>
2025-05-22 17:10:58 +0900

    RSpecをインストールし、不要なファイルが生成されないよう設定し、Minitest用のtestディレクトリを削除し、、ci.ymlのテストをRSpecで実行するよう修正

--- config/application.rb
+++ config/application.rb
@@ -24,6 +24,14 @@ module Backend
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
 
+    config.generators do |g|
+      g.test_framework :rspec,
+        fixtures: false,
+        view_specs: false,
+        helper_specs: false,
+        routing_specs: false
+    end
+
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
---
+++ spec/sanity_check_spec.rb
@@ -0,0 +1,5 @@
+RSpec.describe 'Sanity check' do
+  it '1 + 1 is equal to 2' do
+    expect(1 + 1).to eq(2)
+  end
+end
