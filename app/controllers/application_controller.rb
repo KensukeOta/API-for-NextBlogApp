7f3fb318f00d87507263f91241f881a4c79e9e4d
Kensuke Ota <40653830+KensukeOta@users.noreply.github.com>
2025-06-02 23:28:32 +0900

    記事投稿機能のAPIを実装 (#6)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,24 @@
 class ApplicationController < ActionController::API
+  attr_reader :current_user
+
+  private
+
+    def find_current_user_by_header
+      user_id = request.headers["X-USER-ID"]
+      Rails.logger.debug "X-USER-ID header: #{user_id.inspect}"
+      if user_id.present?
+        user = User.find_by(id: user_id)
+        if user
+          @current_user = user
+        else
+          render_unauthorized
+        end
+      else
+        render_unauthorized
+      end
+    end
+
+    def render_unauthorized
+      render json: { error: "認証が必要です" }, status: :unauthorized
+    end
 end
