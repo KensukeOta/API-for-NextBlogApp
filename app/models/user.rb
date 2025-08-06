78a588c923ee33ade402ba1f47f374aca7a3d3c6
KensukeOta <bump_kensuke_miwa@i.softbank.jp>
2025-08-06 15:33:30 +0900

    Followモデルを作成し、リレーションとバリデーションを実装し、followsテーブルを作成 (#48)

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,6 +13,14 @@ class User < ApplicationRecord
   has_many :sent_messages, class_name: "Message", foreign_key: "from_user_id", dependent: :destroy
   has_many :received_messages, class_name: "Message", foreign_key: "to_user_id", dependent: :destroy
 
+  # フォローしているユーザー達
+  has_many :following_relationships, class_name: "Follow", foreign_key: :follower_id, dependent: :destroy
+  has_many :following, through: :following_relationships, source: :followed
+
+  # 自分をフォローしてくれているユーザー達
+  has_many :follower_relationships, class_name: "Follow", foreign_key: :followed_id, dependent: :destroy
+  has_many :followers, through: :follower_relationships, source: :follower
+
   validates :name, presence: true, uniqueness: true, length: { minimum: 3, maximum: 32 }
   validates :email, presence: true, uniqueness: { scope: :provider }, length: { maximum: 255 }, format: { with: URI::MailTo::EMAIL_REGEXP }
   validates :provider, presence: true
